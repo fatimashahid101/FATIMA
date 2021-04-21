9ee84d34337f06629401241159f47a29484ca466
Fatima Shahid <64368003+fatimashahid101@users.noreply.github.com>
2021-04-22 01:25:17 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+// ======MENU SHOW=====
+const showMenu = (toggleid, navId) =>{
+    const toggle = document.getElementById(toggleid),
+    nav = document.getElementById(navId)
+    if(toggle && nav){
+toggle.addEventListener('click', ()=>{
+    nav.classList.toggle('show')
+})
+    }
+}
+showMenu('nav-toggle','nav-menu')
+// ======remove menu mobile=====
+const navLink = document.querySelectorAll('.nav__link')
+function linkAction(){
+    //active link
+    navLink.forEach(n => n.classList.remove('active'))
+    this.classList.add('active')
+
+    //remove menu link
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
